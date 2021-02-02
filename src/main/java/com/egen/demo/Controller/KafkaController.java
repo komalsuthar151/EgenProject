1d980d3630bc1a190888fdc94489ad08914d6e35
komal <komalsuthar151@gmail.com>
2021-02-02 14:08:48 -0500

    JSON object for order

--- src/main/java/com/egen/demo/Controller/KafkaController.java
+++ src/main/java/com/egen/demo/Controller/KafkaController.java
@@ -1,7 +1,9 @@
 package com.egen.demo.Controller;
 
 import com.egen.demo.Model.Order;
+import com.sun.jmx.remote.util.OrderClassLoaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +16,27 @@ public class KafkaController {
     @Autowired
     KafkaTemplate<String, Order> kafkaTemplate;
 
+    Order orderFromTopic = null;
+
     private static final String TOPIC = "Kafka_Example";
 
     @GetMapping("/publish/{id}")
-    public String post(@PathVariable("id") final String id) {
+    public String post(@PathVariable("id") final String name) {
 
-            kafkaTemplate.send(TOPIC, new Order( "2",  "komal","kkk@gmail.com"));
+        kafkaTemplate.send("Kafka_Example_json", new Order("1", name, name+"@gmail.com"));
 
         return "Published successfully";
     }
 
+    @GetMapping("/publish/orderJSON")
+    public Order consumerOrderJSON() {
+        System.out.println("sending json");
+        return orderFromTopic;
+    }
+
+    @KafkaListener(groupId = "group_json", topics = "Kafka_Example_json", containerFactory = "orderKafkaListenerFactory")
+    public Order getJSONmessage(Order order) {
+        orderFromTopic = order;
+        return orderFromTopic;
+    }
 }
